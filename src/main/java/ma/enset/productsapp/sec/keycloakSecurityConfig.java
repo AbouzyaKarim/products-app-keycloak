59601d2d91ffb978a2aa12278e5843cf34c10613
AbouzyaKarim <kabouzya@gmail.com>
2023-01-11 23:10:14 +0100

    Second Commit

---
+++ src/main/java/ma/enset/productsapp/sec/keycloakSecurityConfig.java
@@ -0,0 +1,40 @@
+package ma.enset.productsapp.sec;
+
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class keycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    // Configuration
+
+    //Droits d'acces
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        http.authorizeRequests().antMatchers("/products/**","/suppliers/**").authenticated();
+
+    }
+
+    //Gestion des users
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+}
